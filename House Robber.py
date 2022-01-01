a3f84c268c47b865e83c6ea328b2464a670b380d
Abbas Shaikh <61411096+Abbas12321@users.noreply.github.com>
2022-01-01 19:44:20 +0530

    Create House Robber.py

---
+++ House Robber.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        rob_current = 0
+        not_rob_current = 0
+
+        for num in nums:
+            new_not_rob_current = max(rob_current, not_rob_current)
+            new_rob_current = not_rob_current + num
+            rob_current = new_rob_current
+            not_rob_current = new_not_rob_current
+        return max(rob_current, not_rob_current)
